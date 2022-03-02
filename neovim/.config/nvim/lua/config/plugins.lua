4fa02ea323a5621945d73c59a0a5f1d8fced1967
Ryan Preston <r.preston73@gmail.com>
2022-03-02 14:58:37 -0500

    feat(norg): add neorg treesitter

--- neovim/.config/nvim/lua/config/plugins.lua
+++ neovim/.config/nvim/lua/config/plugins.lua
@@ -163,7 +163,7 @@ local function init()
 		config = function()
 			require('nvim-treesitter.configs').setup({
 				-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-				ensure_installed = { 'rust', 'go', 'java', 'yaml', 'json', 'lua', 'hcl', 'make', 'python' },
+				ensure_installed = { 'rust', 'go', 'java', 'yaml', 'json', 'lua', 'hcl', 'make', 'python', 'norg' },
 
 				-- Install languages synchronously (only applied to `ensure_installed`)
 				sync_install = false,
