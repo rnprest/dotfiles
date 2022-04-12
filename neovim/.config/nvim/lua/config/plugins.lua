b8d90d939c221abef75d00d848bad4fbf7001bb8
Ryan Preston <r.preston73@gmail.com>
2022-04-12 11:43:22 -0400

    style(plugins): organize

--- neovim/.config/nvim/lua/config/plugins.lua
+++ neovim/.config/nvim/lua/config/plugins.lua
@@ -29,124 +29,67 @@ local function init()
     packer.reset()
 
     use 'wbthomason/packer.nvim' -- Which came first? The chicken or the egg?
-    use 'lewis6991/impatient.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'godlygeek/tabular'
-    use 'rhysd/clever-f.vim' -- Better f-movement - repeat w/ f or F
-    use 'sheerun/vim-polyglot' -- Syntax and other support for almost every programming language
-    use 'tpope/vim-abolish' -- Coercion between snake_case, camelCase, etc. (crs & crc)
-    use 'tpope/vim-fugitive' -- Adds Gread, Gwrite, etc. all of which use buffer
-    use 'tpope/vim-repeat' -- Repeat plugins with '.'
-    use 'tpope/vim-surround' -- Surround words
-    use 'wellle/targets.vim' -- Better text object movement
-    -- Comments
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup {
-                ignore = '^$',
-            }
-        end,
-    }
-    use {
-        's1n7ax/nvim-comment-frame',
-        requires = {
-            { 'nvim-treesitter' },
-        },
-        config = function()
-            require('nvim-comment-frame').setup()
-        end,
-    }
-    ----------
-    use {
-        'windwp/nvim-autopairs',
-        config = function()
-            require('nvim-autopairs').setup()
-        end,
-    }
-    -- Pretty keymaps
+    use 'lewis6991/impatient.nvim' -- Improves startup time for neovim
+
+    ----------------------------------------------------------------------
+    --                              Remaps                              --
+    ----------------------------------------------------------------------
     use {
         'LionC/nest.nvim',
         config = function()
             require 'config.remaps'
         end,
     }
+    ----------------------------------------------------------------------
+    --                               Git                                --
+    ----------------------------------------------------------------------
+    use 'tpope/vim-fugitive'
     use {
-        'kyazdani42/nvim-web-devicons',
+        'lewis6991/gitsigns.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+        },
         config = function()
-            require('nvim-web-devicons').setup()
+            require('gitsigns').setup()
         end,
     }
+    use {
+        'ruifm/gitlinker.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require 'config.gitlinker'
+        end,
+    }
+    ----------------------------------------------------------------------
+    --                           LSP / Syntax                           --
+    ----------------------------------------------------------------------
+    use 'neovim/nvim-lspconfig'
+    use 'sheerun/vim-polyglot' -- Syntax and other support for almost every programming language
+    use {
+        'williamboman/nvim-lsp-installer',
+        event = 'BufEnter',
+        after = 'cmp-nvim-lsp',
+        config = "require('config.lsp')",
+    }
+    use 'tami5/lspsaga.nvim'
+    use 'ray-x/lsp_signature.nvim'
+    use 'simrat39/rust-tools.nvim' -- Inlay-Hints for rust
+    -- " Debugging
+    -- Plug 'nvim-lua/plenary.nvim'
+    -- Plug 'mfussenegger/nvim-dap'
+    ----------------------------------------------------------------------
+    --                            Formatting                            --
+    ----------------------------------------------------------------------
     use 'jose-elias-alvarez/null-ls.nvim'
-    use {
-        'rcarriga/nvim-notify',
-        config = function()
-            require('notify').setup {
-                -- Animation style (see below for details)
-                stages = 'fade_in_slide_out',
-                -- Default timeout for notifications
-                timeout = 1000,
-                -- For stages that change opacity this is treated as the highlight behind the window
-                -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-                background_colour = 'Normal',
-                -- Icons for the different levels
-                icons = {
-                    ERROR = '',
-                    WARN = '',
-                    INFO = '',
-                    DEBUG = '',
-                    TRACE = '✎',
-                },
-            }
-            vim.notify = require 'notify'
-        end,
-    }
-    use {
-        'danymat/neogen',
-        config = function()
-            require('neogen').setup {
-                enabled = true,
-            }
-        end,
-        requires = 'nvim-treesitter/nvim-treesitter',
-    }
-    -- Translate color codes in terminal
-    use {
-        'norcalli/nvim-terminal.lua',
-        config = function()
-            require('terminal').setup()
-        end,
-    }
-    -- Highlight color codes
-    use {
-        'norcalli/nvim-colorizer.lua',
-        config = function()
-            require('colorizer').setup()
-        end,
-    }
-    -- Better increment/decrement
-    use {
-        'monaqa/dial.nvim',
-        config = function()
-            vim.api.nvim_set_keymap('n', '<C-a>', require('dial.map').inc_normal(), { noremap = true })
-            vim.api.nvim_set_keymap('n', '<C-x>', require('dial.map').dec_normal(), { noremap = true })
-            vim.api.nvim_set_keymap('v', '<C-a>', require('dial.map').inc_visual(), { noremap = true })
-            vim.api.nvim_set_keymap('v', '<C-x>', require('dial.map').dec_visual(), { noremap = true })
-            vim.api.nvim_set_keymap('v', 'g<C-a>', require('dial.map').inc_gvisual(), { noremap = true })
-            vim.api.nvim_set_keymap('v', 'g<C-x>', require('dial.map').dec_gvisual(), { noremap = true })
-        end,
-    }
-    -- Treesitter
+    ----------------------------------------------------------------------
+    --                            Treesitter                            --
+    ----------------------------------------------------------------------
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
         config = function()
-            ----------------------------------------------------------------------
-            --                     add 2 parsers for neorg                      --
-            ----------------------------------------------------------------------
+            -- Additional parsers for Neorg (used to take notes)
             local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-            -- These two are optional and provide syntax highlighting
-            -- for Neorg tables and the @document.meta tag
             parser_configs.norg_meta = {
                 install_info = {
                     url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
@@ -162,7 +105,6 @@ local function init()
                 },
             }
             require('nvim-treesitter.configs').setup {
-                -- One of "all", "maintained" (parsers with maintainers), or a list of languages
                 ensure_installed = {
                     'go',
                     'hcl',
@@ -178,21 +120,12 @@ local function init()
                     'toml',
                     'yaml',
                 },
-                -- Install languages synchronously (only applied to `ensure_installed`)
                 sync_install = false,
-
                 indent = {
                     enabled = true,
                 },
-
                 highlight = {
-                    -- `false` will disable the whole extension
                     enable = true,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
                     additional_vim_regex_highlighting = false,
                 },
             }
@@ -206,12 +139,9 @@ local function init()
                 textobjects = {
                     select = {
                         enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
                         lookahead = true,
-
                         keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
+                            -- see after/queries/<language>/textobjects.scm for more capture groups
                             ['af'] = '@function.outer',
                             ['if'] = '@function.inner',
                             ['ac'] = '@class.outer',
@@ -232,46 +162,21 @@ local function init()
         end,
     }
     use 'nvim-treesitter/playground'
-    -- Colorschemes
+    ----------------------------------------------------------------------
+    --                            Telescope                             --
+    ----------------------------------------------------------------------
     use {
-        'marko-cerovac/material.nvim',
-        requires = 'tjdevries/colorbuddy.nvim',
+        'nvim-telescope/telescope.nvim',
+        requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzy-native.nvim' },
         config = function()
-            vim.cmd 'colorscheme material'
-            -- This highlight needs to load AFTER the colorscheme is set so that it isn't overwritten
-            vim.cmd 'highlight CursorLineNr guifg=#fb801a'
-            ----------------------------------------------------------------------
-            --             Remove the underline from LspReference*              --
-            ----------------------------------------------------------------------
-            vim.cmd 'highlight LspReferenceRead gui=NONE guibg=#464B5D'
-            vim.cmd 'highlight LspReferenceWrite gui=NONE guibg=#464B5D'
-            vim.cmd 'highlight LspReferenceText gui=NONE guibg=#464B5D'
+            require 'config.telescope'
         end,
     }
-    -- Statusline
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-        config = function()
-            require('config.statusline').setup()
-        end,
-    }
-    use 'j-hui/fidget.nvim'
-    use {
-        'lewis6991/gitsigns.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-        },
-        config = function()
-            require('gitsigns').setup()
-        end,
-    }
-    -- Code action menu with diffs
-    use {
-        'weilbith/nvim-code-action-menu',
-        cmd = 'CodeActionMenu',
-    }
-    -- LSP
+    use { 'nvim-telescope/telescope-ui-select.nvim', requires = { 'nvim-telescope/telescope.nvim' } }
+    use { 'nvim-telescope/telescope-file-browser.nvim', requires = { 'nvim-telescope/telescope.nvim' } }
+    ----------------------------------------------------------------------
+    --                           Autocomplete                           --
+    ----------------------------------------------------------------------
     use {
         'hrsh7th/nvim-cmp',
         branch = 'dev',
@@ -285,19 +190,9 @@ local function init()
     use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }
     use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }
     use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }
-    use { 'onsails/lspkind-nvim' }
-
-    use {
-        'williamboman/nvim-lsp-installer',
-        event = 'BufEnter',
-        after = 'cmp-nvim-lsp',
-        config = "require('config.lsp')",
-    }
-
-    use { 'tami5/lspsaga.nvim' }
-    use { 'ray-x/lsp_signature.nvim' }
-
-    -- luasnip ❤️   cmp
+    ----------------------------------------------------------------------
+    --                             Snippets                             --
+    ----------------------------------------------------------------------
     use { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' }
     use {
         'L3MON4D3/LuaSnip',
@@ -307,22 +202,135 @@ local function init()
             require 'config.luasnip'
         end,
     }
-    -- Telescope
+    ----------------------------------------------------------------------
+    --                             Display                              --
+    ----------------------------------------------------------------------
     use {
-        'nvim-telescope/telescope.nvim',
-        requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzy-native.nvim' },
+        'kyazdani42/nvim-web-devicons',
         config = function()
-            require 'config.telescope'
+            require('nvim-web-devicons').setup()
         end,
     }
-    use { 'nvim-telescope/telescope-ui-select.nvim', requires = { 'nvim-telescope/telescope.nvim' } }
-    use { 'nvim-telescope/telescope-file-browser.nvim', requires = { 'nvim-telescope/telescope.nvim' } }
-    -- use({ 'ThePrimeagen/git-worktree.nvim', requires = { 'nvim-telescope/telescope.nvim' } })
-    -- Harpoon
     use {
-        'ThePrimeagen/harpoon',
-        requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+        'rcarriga/nvim-notify',
+        config = function()
+            require('notify').setup {
+                stages = 'fade_in_slide_out',
+                timeout = 1000,
+                background_colour = 'Normal',
+                icons = {
+                    ERROR = '',
+                    WARN = '',
+                    INFO = '',
+                    DEBUG = '',
+                    TRACE = '✎',
+                },
+            }
+            vim.notify = require 'notify'
+        end,
     }
+    use 'onsails/lspkind-nvim' -- Adds symbols to lsp completion menu
+    use { -- Highlight color codes
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end,
+    }
+    use {
+        'marko-cerovac/material.nvim',
+        requires = 'tjdevries/colorbuddy.nvim',
+        config = function()
+            vim.cmd 'colorscheme material'
+            -- This highlight needs to load AFTER the colorscheme is set so that it isn't overwritten
+            vim.cmd 'highlight CursorLineNr guifg=#fb801a'
+            -- Remove the underline from LspReference* (for CursorHold aucmd)
+            vim.cmd 'highlight LspReferenceRead gui=NONE guibg=#464B5D'
+            vim.cmd 'highlight LspReferenceWrite gui=NONE guibg=#464B5D'
+            vim.cmd 'highlight LspReferenceText gui=NONE guibg=#464B5D'
+        end,
+    }
+    ----------------------------------------------------------------------
+    --                            Statusline                            --
+    ----------------------------------------------------------------------
+    use { -- Statusline
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function()
+            require('config.statusline').setup()
+        end,
+    }
+    use 'j-hui/fidget.nvim'
+    -- Show breadcrumbs in statusline
+    use {
+        'SmiteshP/nvim-gps',
+        requires = { 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+            require('nvim-gps').setup()
+        end,
+    }
+    ----------------------------------------------------------------------
+    --                        Text Manipulation                         --
+    ----------------------------------------------------------------------
+    use 'wellle/targets.vim' -- Better text object movement
+    use 'tpope/vim-repeat' -- Repeat plugins with '.'
+    use 'tpope/vim-abolish' -- Coercion between snake_case, camelCase, etc. (crs & crc)
+    use 'tpope/vim-surround' -- Surround words
+    use {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup()
+        end,
+    }
+    use { -- Better increment/decrement
+        'monaqa/dial.nvim',
+        config = function()
+            vim.api.nvim_set_keymap('n', '<C-a>', require('dial.map').inc_normal(), { noremap = true })
+            vim.api.nvim_set_keymap('n', '<C-x>', require('dial.map').dec_normal(), { noremap = true })
+            vim.api.nvim_set_keymap('v', '<C-a>', require('dial.map').inc_visual(), { noremap = true })
+            vim.api.nvim_set_keymap('v', '<C-x>', require('dial.map').dec_visual(), { noremap = true })
+            vim.api.nvim_set_keymap('v', 'g<C-a>', require('dial.map').inc_gvisual(), { noremap = true })
+            vim.api.nvim_set_keymap('v', 'g<C-x>', require('dial.map').dec_gvisual(), { noremap = true })
+        end,
+    }
+    use {
+        'ThePrimeagen/refactoring.nvim',
+        requires = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+            require('refactoring').setup {}
+        end,
+    }
+    ----------------------------------------------------------------------
+    --                             Comments                             --
+    ----------------------------------------------------------------------
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup {
+                ignore = '^$',
+            }
+        end,
+    }
+    use {
+        's1n7ax/nvim-comment-frame',
+        requires = {
+            { 'nvim-treesitter' },
+        },
+        config = function()
+            require('nvim-comment-frame').setup()
+        end,
+    }
+    use {
+        'danymat/neogen',
+        config = function()
+            require('neogen').setup {
+                enabled = true,
+            }
+        end,
+        requires = 'nvim-treesitter/nvim-treesitter',
+    }
+    ----------------------------------------------------------------------
+    --                              Notes                               --
+    ----------------------------------------------------------------------
     use {
         'nvim-neorg/neorg',
         tag = '0.0.11',
@@ -366,32 +374,24 @@ local function init()
             }
         end,
     }
-    -- Refactoring
+    ----------------------------------------------------------------------
+    --                          Miscellaneous                           --
+    ----------------------------------------------------------------------
     use {
-        'ThePrimeagen/refactoring.nvim',
-        requires = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-            require('refactoring').setup {}
-        end,
+        'ThePrimeagen/harpoon',
+        requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
     }
-    use {
-        'SmiteshP/nvim-gps',
-        requires = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-            require('nvim-gps').setup()
-        end,
-    }
-    use {
-        'ruifm/gitlinker.nvim',
-        requires = 'nvim-lua/plenary.nvim',
-        config = function()
-            require 'config.gitlinker'
-        end,
-    }
-    -- I'm weak and still need this
-    use 'ThePrimeagen/git-worktree.nvim'
+    -- Worktrees
+    -- use({ 'ThePrimeagen/git-worktree.nvim', requires = { 'nvim-telescope/telescope.nvim' } })
+    -- use 'ThePrimeagen/git-worktree.nvim'
+    -- Documentation
     use 'nanotee/luv-vimdocs'
     use 'milisims/nvim-luaref'
+    -- TODO: Try remaping to this instead of lsp codeaction (remap to :lua CodeActionMenu<CR>)
+    use { -- Code action menu with diffs
+        'weilbith/nvim-code-action-menu',
+        cmd = 'CodeActionMenu',
+    }
 end
 
 local plugins = setmetatable({}, {
