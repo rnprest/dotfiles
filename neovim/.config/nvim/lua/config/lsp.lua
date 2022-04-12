882868e096e98928f40040a4244b274da1eb0635
Ryan Preston <r.preston73@gmail.com>
2022-04-12 11:41:28 -0400

    fix(lsp-installer): make it work with rust-tools

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -83,8 +83,37 @@ lsp_installer.on_server_ready(function(server)
         capabilities = capabilities,
     }
 
-    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-    server:setup(opts)
-    -- I don't think we need this
-    -- vim.cmd([[ do User LspAttachBuffers ]])
+    ----------------------------------------------------------------------
+    --               Integrating nvim-lsp-installer with                --
+    --                         rust-tools.nvim                          --
+    ----------------------------------------------------------------------
+    if server.name == 'rust_analyzer' then
+        opts = {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            tools = {
+                inlay_hints = {
+                    -- prefix for parameter hints
+                    -- default: "<-"
+                    parameter_hints_prefix = '« ',
+                    -- prefix for all the other hints (type, chaining)
+                    -- default: "=>"
+                    other_hints_prefix = '» ',
+                },
+            },
+        }
+        -- Initialize the LSP via rust-tools instead
+        require('rust-tools').setup {
+            -- The "server" property provided in rust-tools setup function are the
+            -- settings rust-tools will provide to lspconfig during init.            --
+            -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+            -- with the user's own settings (opts).
+            server = vim.tbl_deep_extend('force', server:get_default_options(), opts),
+        }
+        server:attach_buffers()
+        -- Only if standalone support is needed
+        require('rust-tools').start_standalone_if_required()
+    else
+        server:setup(opts)
+    end
 end)
