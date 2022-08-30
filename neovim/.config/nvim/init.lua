ddcceca897f63bdee6ae4ed7eaeda8a47566df15
Ryan Preston <r.preston73@gmail.com>
2022-08-30 09:53:12 -0400

    feat: add formatoptions, but they aren't working?

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -18,7 +18,6 @@ local g, opt, cmd = vim.g, vim.opt, vim.cmd
 -----------------------------------------------------------------------------
 g.mapleader = ','
 -----------------
-
 local options = {
     clipboard = 'unnamedplus',
     cmdheight = 2, -- Give more space for displaying messages.
@@ -55,6 +54,17 @@ local options = {
 for k, v in pairs(options) do
     opt[k] = v
 end
+
+opt.formatoptions = opt.formatoptions
+    - 'a' -- Auto formatting is BAD.
+    - 't' -- Don't auto format my code. I got linters for that.
+    + 'c' -- In general, I like it when comments respect textwidth
+    + 'q' -- Allow formatting comments w/ gq
+    - 'o' -- O and o, don't continue comments
+    + 'r' -- But do continue when pressing enter.
+    + 'n' -- Indent past the formatlistpat, not underneath it.
+    + 'j' -- Auto-remove comments if possible.
+    - '2' -- I'm not in gradeschool anymore
 -------------------------------------------------------------------------------
 -- Color settings
 -------------------------------------------------------------------------------
