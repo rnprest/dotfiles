6979b2855cf023e01552eadea0e6e93b2b9b1a53
Ryan Preston <r.preston73@gmail.com>
2023-09-06 08:09:09 -0400

    feat(py): auto-format f-strings when '{' typed

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -335,3 +335,39 @@ vim.api.nvim_create_autocmd('FileType', {
         opt.spell = false
     end,
 })
+
+----------------------------------------------------------------------
+--            Treesitter automatic Python format strings            --
+----------------------------------------------------------------------
+vim.api.nvim_create_augroup('py-fstring', { clear = true })
+vim.api.nvim_create_autocmd('InsertCharPre', {
+    pattern = '*.py',
+    group = 'py-fstring',
+    --- @param opts AutoCmdCallbackOpts
+    --- @return nil
+    callback = function(opts)
+        -- Only run if f-string escape character is typed
+        if vim.v.char ~= '{' then
+            return
+        end
+        -- Get node and return early if not in a string
+        local node = vim.treesitter.get_node()
+        if not node then
+            return
+        end
+        if node:type() ~= 'string' then
+            node = node:parent()
+        end
+        if not node or node:type() ~= 'string' then
+            return
+        end
+        local row, col, _, _ = vim.treesitter.get_node_range(node)
+        -- Return early if string is already a format string
+        local first_char = vim.api.nvim_buf_get_text(opts.buf, row, col, row, col + 1, {})[1]
+        if first_char == 'f' then
+            return
+        end
+        -- Otherwise, make the string a format string
+        vim.api.nvim_input("<Esc>m'" .. row + 1 .. 'gg' .. col + 1 .. "|if<Esc>`'la")
+    end,
+})
