ad6fee4b4fb4f9f33db3241a8ed52df87d008ae0
Ryan Preston <r.preston73@gmail.com>
2025-12-19 09:10:32 -0500

    feat(plugins): add lovely crates.nvim plugin for Cargo.toml

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -38,6 +38,31 @@ return {
     },
 
     {
+        'saecki/crates.nvim',
+        tag = 'stable',
+        config = function()
+            require('crates').setup()
+            local crates = require 'crates'
+            local opts = { silent = true }
+
+            vim.keymap.set('n', '<leader>cv', crates.show_versions_popup, opts)
+            vim.keymap.set('n', '<leader>cF', crates.show_features_popup, opts)
+            vim.keymap.set('n', '<leader>cd', crates.show_dependencies_popup, opts)
+
+            vim.keymap.set('n', '<leader>cu', crates.upgrade_crate, opts)
+            vim.keymap.set('v', '<leader>cu', crates.upgrade_crates, opts)
+            vim.keymap.set('n', '<leader>cA', crates.upgrade_all_crates, opts)
+
+            vim.keymap.set('n', '<leader>cx', crates.expand_plain_crate_to_inline_table, opts)
+
+            vim.keymap.set('n', '<leader>cR', crates.open_repository, opts)
+            vim.keymap.set('n', '<leader>cD', crates.open_documentation, opts)
+            vim.keymap.set('n', '<leader>cC', crates.open_crates_io, opts)
+            vim.keymap.set('n', '<leader>cL', crates.open_lib_rs, opts)
+        end,
+    },
+
+    {
         'folke/snacks.nvim',
         priority = 1000,
         lazy = false,
